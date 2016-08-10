d58045abf617ca6c7be2ca01a4d8493e2fa98ad7
DamonYoung42 <damonyoung@wi.rr.com>
2016-08-10 09:36:05 -0500

    redesigned classes

---
+++ RockPaperScissors/Game.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Game
+    {
+        Player playerOne;
+        Player computerPlayer;
+        bool continuePlaying;
+
+        public Game()
+
+        {
+            computerPlayer = new Player("computer");
+            computerPlayer.SetComputerHand();
+            continuePlaying = true;
+
+        }
+
+        public void RunGame()
+        {
+            SetUser();
+
+            while (continuePlaying) {
+                playerOne.SetChoice();
+                string result = CompareHands();
+                Console.WriteLine(result);
+                Console.WriteLine("Do you want to play again?");
+                if (Console.ReadLine().ToUpper() == "N")
+                {
+                    continuePlaying = false;
+                }
+            }
+
+        }
+
+        public Player SetUser()
+        {
+            Console.WriteLine("Welcome to Rock Paper Scissors");
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            return playerOne = new Player(userName);
+
+        }
+
+        public string CompareHands()
+        {
+            if (playerOne.GetChoice().ToLower() == computerPlayer.GetChoice().ToLower())
+            {
+                return "Both players chose the same " + playerOne.GetChoice().ToLower() + " . It's a tie!";
+            }
+            else
+            {
+                if (playerOne.GetChoice().ToLower() == "scissors")
+                {
+                    if (computerPlayer.GetChoice().ToLower() == "paper")
+                    {
+                        return "Scissors cut paper: " + playerOne.GetName() + " wins!";
+                    }
+                    else
+                    {
+                        return "Rock beats scissors: " + computerPlayer.GetName() + " wins!";
+                    }
+                }
+                else if (playerOne.GetChoice().ToLower() == "rock")
+                {
+                    if (computerPlayer.GetChoice().ToLower() == "scissors")
+                    {
+                        return "Rock beats scissors: " + playerOne.GetName() + " wins!";
+                    }
+                    else
+                    {
+                        return "Paper covers rock: " + computerPlayer.GetName() + " wins!";
+                    }
+                }
+                else if (playerOne.GetChoice().ToLower() == "paper")
+                {
+                    if (computerPlayer.GetChoice().ToLower() == "rock")
+                    {
+                        return "Paper covers rock: " + playerOne.GetName() + " wins!";
+                    }
+                    else
+                    {
+                        return "Scissors cut paper: " + computerPlayer.GetName() + " wins!";
+                    }
+                }
+                else
+                {
+                    return "Winner could not be determined.";
+                }
+            }
+        }
+
+
+    }
+}
---
+++ RockPaperScissors/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Player
+    {
+        string name;
+        string choice;
+
+        public Player(string name)
+        {
+
+            this.name = name;
+        }
+
+        public void SetChoice()
+        {
+            Console.WriteLine("What is your choice -- Rock, Paper or Scissors?");
+            this.choice = Console.ReadLine();
+        }
+
+        public string GetChoice()
+        {
+            return this.choice;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string SetComputerHand()
+        {
+            Random randomNumber = new Random(DateTime.Now.Millisecond);
+            Double randomChoice = randomNumber.NextDouble();
+
+            if (randomChoice <= .33)
+            {
+                return this.choice = "Rock";
+            }
+            else if (randomChoice <= .67)
+            {
+                return this.choice = "Paper";
+            }
+            else
+            {
+                return this.choice = "Scissors";
+            }
+        }
+
+
+    }
+}
